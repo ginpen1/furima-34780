3876b958ccae2da784447b25fb000e65e626378e
ginpen1 <under.bear.thunder.ball@gmail.com>
2021-03-22 14:42:57 +0900

    ActiveHashでカテゴリーの選択を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,28 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one :buyer
-has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
 
-validates :image, :name, :text, :category_id, :state_id, :postage_id, :region_id, :shipping_date_id, :price, presence: true
+  belongs_to :user
+  has_one :buyer
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price
+  end
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :postage_id
+    validates :region_id
+    validates :shipping_date_id
+  end
 
 end
