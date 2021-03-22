aed8a04ef4af13868bd7b166becf45a5f43fed61
ginpen1 <under.bear.thunder.ball@gmail.com>
2021-03-22 11:52:32 +0900

    itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ belongs_to :user
 has_one :buyer
 has_one_attached :image
 
+validates :image, :name, :text, :category_id, :state_id, :postage_id, :region_id, :shipping_date_id, :price, presence: true
+
 end
