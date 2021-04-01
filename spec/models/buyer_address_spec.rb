5882580507578cdf090cec5bacb6b2a0aa4625d3
ginpen1 <under.bear.thunder.ball@gmail.com>
2021-04-01 14:16:51 +0900

    ja.ymlファイルを作成

--- spec/models/buyer_address_spec.rb
+++ spec/models/buyer_address_spec.rb
@@ -22,62 +22,62 @@ RSpec.describe BuyerAddress, type: :model do
       it 'postal_codeが空では保存できない' do
         @buyer_address.postal_code = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("Postal code can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("郵便番号を入力してください")
       end
       it 'region_idが空では保存できない' do
         @buyer_address.region_id = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("Region can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("都道府県を入力してください")
       end
       it 'cityが空では保存できない' do
         @buyer_address.city = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("City can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("市町村区を入力してください")
       end
       it 'address_lineが空では保存できない' do
         @buyer_address.address_line = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("Address line can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("番地を入力してください")
       end
       it 'phone_numberが空では保存できない' do
         @buyer_address.phone_number = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("Phone number can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("電話番号を入力してください")
       end
       it 'user_idが空では保存できない' do
         @buyer_address.user_id = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("User can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("Userを入力してください")
       end
       it 'item_idが空では保存できない' do
         @buyer_address.item_id = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("Item can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("Itemを入力してください")
       end
       it 'tokenが空では保存できない' do
         @buyer_address.token = nil
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include("Token can't be blank")
+        expect(@buyer_address.errors.full_messages).to include("クレジットカード情報を入力してください")
       end
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
         @buyer_address.postal_code = '1234567'
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include('Postal code is invalid')
+        expect(@buyer_address.errors.full_messages).to include('郵便番号は不正な値です')
       end
       it 'phone_numberに数値以外が含まれていると保存できない' do
         @buyer_address.phone_number = '000-1234-5678'
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include('Phone number is not a number')
+        expect(@buyer_address.errors.full_messages).to include('電話番号は不正な値です')
       end
       it 'phone_numberが12桁以上では保存できない' do
         @buyer_address.phone_number = '123456789012'
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include('Phone number is invalid')
+        expect(@buyer_address.errors.full_messages).to include('電話番号は不正な値です')
       end
       it 'region_idが1では登録できない' do
         @buyer_address.region_id = 1
         @buyer_address.valid?
-        expect(@buyer_address.errors.full_messages).to include('Region must be other than 1')
+        expect(@buyer_address.errors.full_messages).to include('都道府県は1以外の値にしてください')
       end
     end
   end
